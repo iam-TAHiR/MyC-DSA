93a9d7e3822d2457c96a8a65e0754280e8b9cb6e
Mohammed Tahir <twahirmohammed786@gmail.com>
2025-11-06 15:31:16 +0530

    linked list with insert and delete end  functions

---
+++ del_end_LL.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* head=NULL;
+void insertend(int val)
+{
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=val;
+    newnode->next=NULL;
+    if(head==NULL)
+    {
+        newnode->next=head;
+        head=newnode;
+    }
+    else
+    {
+        struct node* temp=head;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+    
+}
+void delend()
+{
+    if(head==NULL)
+    {
+        printf("The list is empty\n");
+    }
+    struct node* temp=head;
+    head=head->next;
+    printf("The deleted element %d\n",temp->data);
+    free(temp);
+}
+void display()
+{
+    struct node* temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+printf("NULL\n");
+
+}
+int main()
+{
+    insertend(10);
+    insertend(20);
+    insertend(30);
+    insertend(40);
+    display();
+    delend();
+    display();
+}
